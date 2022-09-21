814cf096f2b0dfaf60f8c27b7e9cd16c6f36e589
Shrikant <shrikantp0017@gmail.com>
2022-09-21 20:31:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import {Map, Marker} from 'react-map-gl'
 function App() {
+  let [viewPort, SetViewPort] = useState({
+    longitude: -122.4,
+    latitude: 37.8,
+    zoom: 8,
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='app'>
+      <div style={{backgroundColor:'ButtonHighlight' ,position:'absolute' ,height:'40px',width:'40vw',padding:'10px',zIndex:'1',color:'black'}}>
+         longitude: {viewPort.longitude} || 
+         latitude: {viewPort.latitude} || 
+         zoom:{viewPort.zoom}</div>
+   <Map 
+    mapboxAccessToken='pk.eyJ1Ijoic2hyaXBhdGlsIiwiYSI6ImNsM2pza2QwMTF6NDYzY3JzZTE2cTh5djIifQ.ZnWMGFm9MUaScMLeXvgV9g'
+   {...viewPort} 
+   onMove={evt => {
+      SetViewPort(evt.viewState)}}
+   mapStyle="mapbox://styles/mapbox/streets-v9"
+   style={{width:'100vw',height:'100vh',position:'relative'}}
+    
+   />
+</div>
   );
 }
 
